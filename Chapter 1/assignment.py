ca8db163330f7ce24cd925d6121022a4a4e0432b
Parag Debnath <94636114+parag-debnath@users.noreply.github.com>
2026-02-28 14:45:30 +0530

    Add files via upload

---
+++ Chapter 1/assignment.py
@@ -0,0 +1,5 @@
+# take input from the user and calculate the area of a circle
+D = input("Tell me the diameter of a circle")
+radius = float(D)/2
+area = 3.14 * radius * radius
+print("The area of a circle is :" , area)
