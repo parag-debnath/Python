443d7b5c15e603d6c66491293c6cb41682362bab
Parag Debnath <94636114+parag-debnath@users.noreply.github.com>
2026-02-28 13:11:39 +0530

    Add files via upload

---
+++ Chapter 1/codeExecution.py
@@ -0,0 +1,7 @@
+name ="Parag"
+age1 = 18
+age2 = 40
+print("my actual age is " , age2)
+age2 = age1
+print("my changed age is ", age2)
+
---
+++ Chapter 1/userinput.py
@@ -0,0 +1,5 @@
+# Programmme to make the input from the user 
+name = input("What is your name ? ")
+age = input("What is your age ?")
+print(" your name is : " , name)
+print(" Your age is :" , age )
